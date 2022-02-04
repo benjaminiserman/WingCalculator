f6a1a8e6ad032e12e47ad03c0da68e871784f59c
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-04 00:21:04 -0600

    change MacroPointerNode to return 1 if set

--- WingCalculatorShared/AssignmentNode.cs
+++ WingCalculatorShared/AssignmentNode.cs
@@ -6,7 +6,7 @@ internal record AssignmentNode(IAssignable A, INode B) : INode
 	{
 		A.Assign(B);
 
-		if (A is MacroNode) return 1;
+		if (A is MacroNode or MacroPointerNode) return 1;
 		else return ((INode)A).Solve(); // probably shouldn't double-dip
 	}
 }

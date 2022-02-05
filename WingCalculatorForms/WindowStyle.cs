51328da231782199eb9885b4de8c5745942e7fb6
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-04 18:22:41 -0600

    add dark mode

---
+++ WingCalculatorForms/WindowStyle.cs
@@ -0,0 +1,67 @@
+﻿namespace WingCalculatorForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+internal record WindowStyle(Color BaseColor, Color InnerBaseColor, Color ButtonBackgroundColor, Color TextColor, Color ErrorColor, Color InfoColor, FlatStyle ButtonStyle, FlatStyle MenuButtonStyle)
+{
+	public static readonly Color DarkBase = Hex("#121212");
+
+	public static readonly WindowStyle DarkMode = new
+	(
+		BaseColor: DarkBase,
+		InnerBaseColor: ControlPaint.Light(DarkBase, 0.1f),
+		ButtonBackgroundColor: ControlPaint.Light(DarkBase, 0.2f),
+		TextColor: Color.White,
+		ErrorColor: Color.IndianRed,
+		InfoColor: Color.LightSkyBlue,
+		ButtonStyle: FlatStyle.Popup,
+		MenuButtonStyle: FlatStyle.Flat
+	);
+
+	public static readonly WindowStyle LightMode = new
+	(
+		BaseColor: Color.FromKnownColor(KnownColor.Control),
+		InnerBaseColor: Color.FromKnownColor(KnownColor.Control),
+		ButtonBackgroundColor: Color.FromKnownColor(KnownColor.ControlLight),
+		TextColor: Color.Black,
+		ErrorColor: Color.Red,
+		InfoColor: Color.DarkBlue,
+		ButtonStyle: FlatStyle.Standard,
+		MenuButtonStyle: FlatStyle.Standard
+	);
+
+	public static List<string> MenuNames { get; } = new() { "darkModeButton" };
+
+	public void Apply(Control.ControlCollection collection, Form form)
+	{
+		foreach (Control c in collection)
+		{
+			if (c is Button b)
+			{
+				c.BackColor = ButtonBackgroundColor;
+
+				if (MenuNames.Contains(b.Name)) b.FlatStyle = MenuButtonStyle;
+				else b.FlatStyle = ButtonStyle;
+			}
+			else if (c is TextBox)
+			{
+				c.BackColor = InnerBaseColor;
+			}
+			else if (c is ListBox)
+			{
+				c.BackColor = InnerBaseColor;
+			}
+
+			if (c.Name == "errorLabel") c.ForeColor = ErrorColor;
+			else c.ForeColor = TextColor;
+		}
+
+		form.BackColor = BaseColor;
+	}
+
+	private static Color Hex(string s) => ColorTranslator.FromHtml(s);
+}

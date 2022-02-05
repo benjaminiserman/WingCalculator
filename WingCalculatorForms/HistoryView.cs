96cf1c1d6eca0fc6432c8541dd8ef2826216dacc
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-05 16:36:36 -0600

    cleanup

--- WingCalculatorForms/HistoryView.cs
+++ WingCalculatorForms/HistoryView.cs
@@ -1,10 +1,4 @@
 ﻿namespace WingCalculatorForms;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 internal class HistoryView : ListBox
 {
 	public HistoryView()

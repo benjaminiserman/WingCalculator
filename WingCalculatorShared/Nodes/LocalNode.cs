a4ba3991025a568471627439f9504ce49cb50293
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-07 14:47:14 -0600

    initial implemenation for LocalNode... probably needs more thought to it

---
+++ WingCalculatorShared/Nodes/LocalNode.cs
@@ -0,0 +1,19 @@
+﻿namespace WingCalculatorShared;
+
+internal record LocalNode(int Index, List<INode> Locals) : INode, IAssignable
+{
+	public double Solve() => Locals[Index].Solve();
+
+	public double Assign(INode a)
+	{
+		Locals[Index] = a;
+		return 1;
+	}
+
+	public double Assign(double a)
+	{
+		Locals[Index] = new ConstantNode(a);
+		return 1;
+	}
+}
+

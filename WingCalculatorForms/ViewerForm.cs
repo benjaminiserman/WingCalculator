fa79ba7706a0af97addfde28b87fde93f97b7732
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-08 16:29:48 -0600

    add ViewerForm.Refresh

--- WingCalculatorForms/ViewerForm.cs
+++ WingCalculatorForms/ViewerForm.cs
@@ -1,18 +1,44 @@
 ﻿namespace WingCalculatorForms;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using WingCalculatorShared;
 
 public partial class ViewerForm : Form
 {
 	public ViewerForm()
 	{
 		InitializeComponent();
+	}
+
+	public void Refresh(Solver solver)
+	{
+		variableView.Items.Clear();
+
+		bool allcaps = allcapsCheck.Checked;
+		bool pointers = pointersCheck.Checked;
+		bool zeros = zerosCheck.Checked;
+		
+		foreach ((string key, double val) in solver.GetValues())
+		{
+			if (double.TryParse(key, out _))
+			{
+				if (pointers) AddItem(key, val, zeros);
+			}
+			else if (key.All(c => char.IsUpper(c)))
+			{
+				if (allcaps) AddItem(key, val, zeros);
+			}
+			else
+			{
+				AddItem(key, val, zeros);
+			}
+		}
+	}
+
+	private void AddItem(string key, double val, bool zeros)
+	{
+		if (val != 0 || zeros)
+		{
+			variableView.Items.Add($"${key} = {val}");
+		}
 	}
 }

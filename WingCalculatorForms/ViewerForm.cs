1a631e18992a5eb83137abc4c2ee2be376231f98
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-08 16:47:22 -0600

    constants show even if 0, NAN is not a pointer

--- WingCalculatorForms/ViewerForm.cs
+++ WingCalculatorForms/ViewerForm.cs
@@ -4,6 +4,8 @@ using WingCalculatorShared;
 
 public partial class ViewerForm : Form
 {
+	private Solver _solver;
+
 	public ViewerForm()
 	{
 		InitializeComponent();
@@ -11,21 +13,28 @@ public partial class ViewerForm : Form
 
 	public void RefreshEntries(Solver solver)
 	{
+		_solver = solver;
+
+		RefreshEntries();
+	}
+
+	public void RefreshEntries()
+	{
 		variableView.Items.Clear();
 
 		bool allcaps = allcapsCheck.Checked;
 		bool pointers = pointersCheck.Checked;
 		bool zeros = zerosCheck.Checked;
-		
-		foreach ((string key, double val) in solver.GetValues())
+
+		foreach ((string key, double val) in _solver.GetValues())
 		{
-			if (double.TryParse(key, out _))
+			if (key != "NAN" && double.TryParse(key, out _)) // NAN gets parsed as a double
 			{
 				if (pointers) AddItem(key, val, zeros);
 			}
 			else if (key.All(c => char.IsUpper(c)))
 			{
-				if (allcaps) AddItem(key, val, zeros);
+				if (allcaps) AddItem(key, val, true); // true because allcaps should be shown even if == 0
 			}
 			else
 			{
@@ -47,4 +56,8 @@ public partial class ViewerForm : Form
 		Hide();
 		e.Cancel = true; // this cancels the close event.
 	}
+
+	private void allcapsCheck_CheckedChanged(object sender, EventArgs e) => RefreshEntries();
+	private void pointersCheck_CheckedChanged(object sender, EventArgs e) => RefreshEntries();
+	private void zerosCheck_CheckedChanged(object sender, EventArgs e) => RefreshEntries();
 }

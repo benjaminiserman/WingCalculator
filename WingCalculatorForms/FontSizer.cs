95db128649bf6893f598f54d8516bb710b95702c
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-11 13:36:11 -0600

    add FontSizer.cs, part of #45

---
+++ WingCalculatorForms/FontSizer.cs
@@ -0,0 +1,22 @@
+﻿namespace WingCalculatorForms;
+
+internal static class FontSizer
+{
+	public static void ApplySize(Control.ControlCollection collection, Form form, float size)
+	{
+		foreach (Control control in collection)
+		{
+			if (control is TextBox or HistoryView or Label)
+			{
+				control.Font = new(control.Font.FontFamily, size);
+			}
+
+			if (control is Form f) ApplySize(collection, f, size);
+		}
+
+		if (form is MainForm mf)
+		{
+			ApplySize(mf.ViewerForm.Controls, mf.ViewerForm, size);
+		}
+	}
+}

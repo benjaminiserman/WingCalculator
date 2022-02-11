5785f7e3b704db8d2f5a3361664a319120b3b7b5
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-11 14:26:39 -0600

    resolve #45

--- WingCalculatorForms/FontSizer.cs
+++ WingCalculatorForms/FontSizer.cs
@@ -1,17 +1,15 @@
 ﻿namespace WingCalculatorForms;
+using System.Windows.Forms;
 
 internal static class FontSizer
 {
 	public static void ApplySize(Control.ControlCollection collection, Form form, float size)
 	{
-		foreach (Control control in collection)
+		foreach (Control c in collection)
 		{
-			if (control is TextBox or HistoryView or Label)
-			{
-				control.Font = new(control.Font.FontFamily, size);
-			}
-
-			if (control is Form f) ApplySize(collection, f, size);
+			if (c is TextBox or Label or ListBox or CheckBox) c.Font = new(c.Font.FontFamily, size);
+			if (c is HistoryView hv) hv.RefreshEntries();	
+			if (c is Panel panel) ApplySize(panel.Controls, form, size);	
 		}
 
 		if (form is MainForm mf)

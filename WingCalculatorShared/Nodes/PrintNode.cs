b1e217c3fc28b7360adf04c22dd130bb0bce9504
Benjamin E. Iserman <winggar1228@gmail.com>
2022-02-18 19:06:53 -0600

    add FRAC print mode, resolve #14

--- WingCalculatorShared/Nodes/PrintNode.cs
+++ WingCalculatorShared/Nodes/PrintNode.cs
@@ -29,7 +29,7 @@ internal record PrintNode(INode A, INode B, Solver Solver, bool Newline) : INode
 		1 => ((char)(int)x).ToString(),
 		2 => Convert.ToString((int)x, 2),
 		3 => $"{x:P}",
-		4 => throw new NotImplementedException("FRAC format is not yet implemented."),
+		4 => GetFraction(x),
 		5 => $"{x:F}",
 		8 => Convert.ToString((int)x, 8),
 		16 => Convert.ToString((int)x, 16),
@@ -37,4 +37,43 @@ internal record PrintNode(INode A, INode B, Solver Solver, bool Newline) : INode
 
 		_ => throw new NotImplementedException($"Format {y} is not implemented.")
 	};
+
+	// adapted from the Stern-Brocot tree traversal algorithm described here: https://stackoverflow.com/questions/5124743/algorithm-for-simplifying-decimal-to-fractions
+	private static string GetFraction(double x, double error = 1e-20)
+	{
+		int n = (int)Math.Floor(x);
+
+		x -= n;
+
+		if (x < error) return $"{n}";
+		else if (1 - error < x) return $"{n + 1}";
+
+		int lowerN = 0;
+		int lowerD = 1;
+
+		int upperN = 1;
+		int upperD = 1;
+
+		while (true)
+		{
+			int middleN = lowerN + upperN;
+			int middleD = lowerD + upperD;
+
+			if (middleD * (x + error) < middleN)
+			{
+				upperN = middleN;
+				upperD = middleD;
+			}
+			else if (middleN < (x - error) * middleD)
+			{
+				lowerN = middleN;
+				lowerD = middleD;
+			}
+			else
+			{
+				if (n == 0) return $"{middleN} / {middleD}";
+				else return $"{n * middleD + middleN} / {middleD}\nOR\n{n} + ({middleN} / {middleD})";
+			}
+		}
+	}
 }
